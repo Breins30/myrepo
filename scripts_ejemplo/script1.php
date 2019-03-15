9472a094af39e4fac26c8f8dc66eba3ec8639200
breins30 <brayanramirez2000@gmail.com>
2019-03-15 11:59:44 -0500

    Primera clase php

---
+++ scripts_ejemplo/script1.php
@@ -0,0 +1,18 @@
+<center>Programacion avanzada</center>
+
+<?php
+
+	$nombre = "peter";
+	$apellido = "parker";
+	$edad = 30;
+	$salario = 800;
+	
+	echo "<br><center>Bienvenidos</center>";
+	echo "<br>Nombre: ". $nombre;
+	echo "<br>Apellido: ". $apellido;
+	echo "<br>Edad: ". $edad;
+	echo "<br>Salario: ". $salario ."<br>";
+
+?>
+
+<center>Fin del programa</center>
---
+++ scripts_ejemplo/script2.php
@@ -0,0 +1,18 @@
+<center><h1>Calculadora basica</h1></center><br>
+
+<?php
+
+	$num1= 10;
+	$num2= 5;
+	
+	/*$sum=$num1+$num2;
+	$res=$num1-$num2;
+	$mul=$num1*$num2;
+	$div=$num1/$num2;*/
+	
+	echo "<center>La suma es: ". ($num1+$num2);
+	echo "<br>La resta es: ". ($num1-$num2);
+	echo "<br>La multiplicacion es: ". ($num1*$num2);
+	echo "<br>La division es: ". ($num1/$num2) ."</center>";
+
+?>
